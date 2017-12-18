9446eb164d2d872ed62cfe7cc36dca90df56ee10
tristanwqy <tristanwqy@github.com>
2017-12-18 17:18:44 +0800

    2

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import sys
-
+import os
 from cx_Freeze import Executable, setup
+
+os.environ["TCL_LIBRARY"] = 'c:\\Anaconda3\\tcl\\tcl8.6'
+os.environ["TK_LIBRARY"] = 'c:\\Anaconda3\\tcl\\tk8.6'
 
 base = None
 if sys.platform == "win32":
